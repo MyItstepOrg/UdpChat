a681aec99b99d17ac2b95a53d2edbed77d355161
hash1riya <daniildrok1@gmail.com>
2024-09-03 20:34:49 +0300

    Init

---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using UdpChat.ViewModel;
+
+namespace UdpChat.View;
+public partial class MainPage : ContentPage
+{
+    public MainPage(MainViewModel vm)
+    {
+        InitializeComponent();
+        this.BindingContext = vm;
+    }
+}
